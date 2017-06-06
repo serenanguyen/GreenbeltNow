f09c96f534f3b1317efc01c48c7e6fd62b0ebd78
Your Name <s.nguyen11@utexas.edu>
2017-06-06 16:33:46 -0500

    img-responsive

--- app/components/Header.js
+++ app/components/Header.js
@@ -7,7 +7,7 @@ class Header extends React.Component {
         return(
             <div className="fadeInDown">
                 <h1>GREENBELT NOW</h1>
-                <img src="https://i.imgur.com/5xW6mRh.png"/>
+                <img src="https://i.imgur.com/5xW6mRh.png" className="img-responsive"/>
                 <h2>Can I swim at the Greenbelt today</h2>
                 <p>Select a Greenbelt access point from the dropdown menu to begin your search.</p>
 
