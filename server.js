901368b72cfd94fa710a9a08142cf5c5cc4b6707
Your Name <s.nguyen11@utexas.edu>
2017-06-22 17:28:40 -0500

    connection

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-var connectionString = require("./connectionString");
 
 // weather
 var weather = require('weather-js');
